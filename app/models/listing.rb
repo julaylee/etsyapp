7a2528d8a64cc77c09c70b296a174eff538cc5f9
Julie Hagen <julierosehagen@gmail.com>
2016-09-26 16:53:51 -0500

    tried to fix problem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,13 @@ class Listing < ActiveRecord::Base
   if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
 
-end
+      else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+                    
+                      :path => ":style/:id_:filename"
+  end
+
+
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
