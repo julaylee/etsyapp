8db55fbd0ae3132b9db83a2f365d5f5283861e3a
Julie Hagen <julierosehagen@gmail.com>
2016-10-01 14:28:06 -0500

    trying to fix Users/listings. Deleting old listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_filter :check_user, only: [:edit, :update, :destroy]
+
   # GET /listings
   # GET /listings.json
   def index
