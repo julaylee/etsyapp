6475e6eafb29b56fa016e001bae92bd634783cf2
Julie Hagen <julierosehagen@gmail.com>
2016-10-01 13:49:52 -0500

    trying to fix things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -8,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up)
-    devise_parameter_sanitizer.permit(:account_update)
+    devise_parameter_sanitizer.for(:sign_up) {:name}
+    devise_parameter_sanitizer.for(:account_update) {:name}
   end
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-
-
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
   # GET /listings
   # GET /listings.json
   def index
@@ -24,7 +24,8 @@ class ListingsController < ApplicationController
 
   # POST /listings
   # POST /listings.json
-
+  def create
+    @listing = Listing.new(listing_params)
 
     respond_to do |format|
       if @listing.save
@@ -72,3 +73,10 @@ class ListingsController < ApplicationController
       params.require(:listing).permit(:name, :description, :price, :image)
     end
 
+
+    def check_user
+      if current_user != @listing.user
+        redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
+      end
+    end
+end
